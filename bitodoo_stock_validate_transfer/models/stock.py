e557aad2c809088f08f7734d452237f598a77d81
Wilder <wilderhernandezg@gmail.com>
2018-06-01 14:14:48 -0500

    [space] .join

--- bitodoo_stock_validate_transfer/models/stock.py
+++ bitodoo_stock_validate_transfer/models/stock.py
@@ -12,9 +12,9 @@ class StockPicking(models.Model):
         location_dest_names = [x.location_dest_id.name for x in self.move_lines if x.location_dest_id.id != self.location_dest_id.id]
         if location_origin_names:
             msm = u"las ubicaciones de origén [{}] son diferentes a la ubicación de la transferencia ({})"
-            raise Warning(msm.format(''.join(location_origin_names), self.location_id.name))
+            raise Warning(msm.format(', '.join(location_origin_names), self.location_id.name))
         if location_dest_names:
             msm = u"las ubicaciones [{}] son diferentes a la ubicación de la transferencia ({})"
-            raise Warning(msm.format(''.join(location_dest_names), self.location_dest_id.name))
+            raise Warning(msm.format(', '.join(location_dest_names), self.location_dest_id.name))
         res = super(StockPicking, self).action_assign()
         return res
