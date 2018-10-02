6ea0eb839e5783f50574b4822c8ce8248aceefda
dperalta <david.2964.erls@gmail.com>
2018-10-02 00:58:46 -0500

    Add new modules

---
+++ bitodoo_product_attr/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Bitodoo product attribute",
+
+    'summary': """Bitodoo product attribute""",
+
+    'description': """
+    # Add fields in product
+
+    - Colección
+    - Modelo
+    - Composición
+    - Nombre comercial
+    - Temporada
+    - Etc.
+    """,
+    'author': "Bitodoo",
+    'website': "http://www.bitodoo.com",
+    'category': 'Others',
+    'version': '0.1',
+
+    'depends': ['product', 'bitodoo_datas'],
+
+    'data': [
+        'views/product_views.xml',
+    ],
+
+    'demo': [
+    ],
+    'application': True,
+}
