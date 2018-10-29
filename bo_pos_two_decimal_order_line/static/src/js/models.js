5b0f233ca96a0d0b29c93f77f76bde39297a3466
dperalta <david.2964.erls@gmail.com>
2018-10-28 22:24:59 -0500

    Add new module bo_pos_two_decimal_order_line

---
+++ bo_pos_two_decimal_order_line/static/src/js/models.js
@@ -0,0 +1,32 @@
+odoo.define('bo_pos_two_decimal_order_line.models', function (require) {
+"use strict";
+
+    var rpc = require('web.rpc');
+    var models = require('point_of_sale.models');
+
+    var PosModelSuper = models.PosModel.prototype;
+    models.PosModel = models.PosModel.extend({
+        load_server_data: function(){
+            var self = this;
+
+            var loaded = PosModelSuper.load_server_data.call(this);
+
+            var currency_model = _.find(this.models, function(model){
+                return model.model === 'res.currency';
+            });
+            if (currency_model) {
+                var loaded_super = currency_model.loaded;
+                currency_model.loaded = function(self, currencies){
+                    // loaded_super(self, currencies);
+                    self.currency = currencies[0];
+                    if (self.currency.rounding > 0 && self.currency.rounding < 1) {
+                        self.currency.decimals = 2;
+                    } else {
+                        self.currency.decimals = 0;
+                    }
+                };
+            }
+            return loaded;
+        },
+    });
+});
