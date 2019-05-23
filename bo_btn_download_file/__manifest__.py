4e682e3bc311fcc573701dbf2008822a2bbeccf7
dperalta <david.2964.erls@gmail.com>
2019-05-23 18:29:53 -0500

    Add new module bo_btn_download_file

---
+++ bo_btn_download_file/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018, AUTHOR(S) : Bitodoo
+{
+    'name': 'Bitodoo action button download',
+    'summary': 'Bitodoo action button download',
+    'version': '11.0',
+    'category': 'Tools',
+    'website': 'https://bitodoo.com/',
+    'author': 'Bitodoo',
+    'depends': ['base'],
+    'data': [
+        # 'views/hr_views.xml',
+        # 'views/stock_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ bo_btn_download_file/controllers/main.py
@@ -0,0 +1,43 @@
+from odoo.tools import html_escape
+import json
+
+from odoo import http
+from odoo.http import content_disposition, request
+from odoo.addons.web.controllers.main import serialize_exception
+
+
+class Binary(http.Controller):
+    @http.route('/web/download/binary_content', type='http', auth="public")
+    @serialize_exception
+    def download_binary_content(self, model, method, id, filename='download_file.xls',
+                                mimetype='application/octet-stream;charset=utf-8;', **kw):
+        if not model or not method or not id or not filename or not mimetype:
+            error = {
+                'code': 200,
+                'message': "Odoo Server Error",
+                'data': "Not params"}
+            return request.make_response(html_escape(json.dumps(error)))
+
+        if not request.env['ir.model'].sudo().search([('model', '=', model)]):
+            error = {
+                'code': 200,
+                'message': "Odoo Server Error",
+                'data': "Not exist model"}
+            return request.make_response(html_escape(json.dumps(error)))
+        Model = request.env[model]
+        if not hasattr(Model, method):
+            error = {
+                'code': 200,
+                'message': "Odoo Server Error",
+                'data': "Not method"}
+            return request.make_response(html_escape(json.dumps(error)))
+
+        data = getattr(Model.browse(id), method)()
+
+        headers = [
+            ('Content-Type', mimetype),
+            ('Content-Length', len(data)),
+            ('Content-Disposition', content_disposition(filename))
+        ]
+
+        return request.make_response(data, headers=headers)
