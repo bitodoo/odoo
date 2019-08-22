a999d0562995c5c243834f21367ac83fffb59186
dperalta <david.2964.erls@gmail.com>
2019-08-22 15:57:09 -0500

    Add new module bo_pos_two_decimal_order_line

---
+++ bo_pos_two_decimal_order_line/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Pos two decimal order line',
+    'sequence': 0,
+    'version': '0.1',
+    'author': 'Bitodoo',
+    'description': 'Pos two decimal order line',
+    'category': 'Point of Sale',
+    'depends': ['point_of_sale'],
+    'data': [
+        'template.xml',
+    ],
+    'price': 15.00,
+    'currency': 'EUR',
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    'installable': True,
+    'application': True,
+    'website': 'http://bitodoo.com',
+    'support': 'bitodoo@gmail.com'
+}
