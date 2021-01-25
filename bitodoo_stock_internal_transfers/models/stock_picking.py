a4653211ff2c407b3d1d0e90277446b8ff1d18db
wilderhernandezg@gmail.com <wilderhernandezg@gmail.com>
2021-01-25 15:12:17 -0500

    [IMP]: validation transfer internal

--- bitodoo_stock_internal_transfers/models/stock_picking.py
+++ bitodoo_stock_internal_transfers/models/stock_picking.py
@@ -44,3 +44,41 @@ class StockPicking(models.Model):
         
         for rec in self:
             rec.state = 'assigned'
+    
+    @api.multi
+    def button_validate(self):
+        res = super(StockPicking, self).button_validate()
+        if self.picking_type_id.code == "internal":
+            for line in self.move_lines:
+                product_name = line.product_id.display_name
+                if not line.product_id.type == 'product':
+                    raise Warning(u"El producto {} debe ser de tipo Almacenable.".format(product_name))
+                if self.location_id.id:
+                    obj_sq = self.env['stock.quant'].search([
+                                                            ('location_id', '=', self.location_id.id),
+                                                            ('product_id', '=', line.product_id.id)
+                                                            ])
+                    product_stock_qty = sum([x.quantity for x in obj_sq if x.quantity])
+                    product_qty = line.quantity_done
+                    name_warehouse = self.location_id.name
+                    if product_qty > product_stock_qty and self.picking_type_id.code != 'incoming':
+                        messaje = u"Usted planea transferir {} {} pero solo tiene {} en {} ".format(product_qty, product_name, product_stock_qty, name_warehouse)
+                        raise Warning(messaje)
+
+            for line in self.move_line_ids:
+                product_name = line.product_id.display_name
+                if not line.product_id.type == 'product':
+                    raise Warning(u"El producto {} debe ser de tipo Almacenable.".format(product_name))
+                if self.location_id.id:
+                    obj_sq = self.env['stock.quant'].search([
+                                                            ('location_id', '=', self.location_id.id),
+                                                            ('product_id', '=', line.product_id.id)
+                                                            ])
+                    product_stock_qty = sum([x.quantity for x in obj_sq if x.quantity])
+                    product_qty = line.qty_done
+                    name_warehouse = self.location_id.name
+                    if product_qty > product_stock_qty and self.picking_type_id.code != 'incoming':
+                        messaje = u"Usted planea transferir {} {} pero solo tiene {} en {} ".format(product_qty, product_name, product_stock_qty, name_warehouse)
+                        raise Warning(messaje)
+
+        return res
