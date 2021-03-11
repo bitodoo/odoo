9d35ec4ba829b03cbed7f8805349004d32a35587
Bitodoo <bitodoo@gmail.com>
2021-03-10 19:33:50 -0500

    [IMP] QTY

--- bitodoo_stock_internal_transfers/models/stock_picking.py
+++ bitodoo_stock_internal_transfers/models/stock_picking.py
@@ -9,17 +9,12 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
     
     def state_list(self):
-
         for line in self.move_lines:
             product_name = line.product_id.display_name
             if not line.product_id.type == 'product':
                 raise Warning(u"El producto {} debe ser de tipo Almacenable.".format(product_name))
             if self.location_id.id:
-                obj_sq = self.env['stock.quant'].search([
-                                                        ('location_id', '=', self.location_id.id),
-                                                        ('product_id', '=', line.product_id.id)
-                                                        ])
-                product_stock_qty = sum([x.quantity for x in obj_sq if x.quantity])
+                product_stock_qty = line.product_id.with_context({'location': self.location_id.id}).qty_available
                 product_qty = line.quantity_done
                 name_warehouse = self.location_id.name
                 if product_qty > product_stock_qty and self.picking_type_id.code != 'incoming':
@@ -31,11 +26,7 @@ class StockPicking(models.Model):
             if not line.product_id.type == 'product':
                 raise Warning(u"El producto {} debe ser de tipo Almacenable.".format(product_name))
             if self.location_id.id:
-                obj_sq = self.env['stock.quant'].search([
-                                                        ('location_id', '=', self.location_id.id),
-                                                        ('product_id', '=', line.product_id.id)
-                                                        ])
-                product_stock_qty = sum([x.quantity for x in obj_sq if x.quantity])
+                product_stock_qty = line.product_id.with_context({'location': self.location_id.id}).qty_available
                 product_qty = line.qty_done
                 name_warehouse = self.location_id.name
                 if product_qty > product_stock_qty and self.picking_type_id.code != 'incoming':
@@ -55,22 +46,17 @@ class StockPicking(models.Model):
 
     @api.multi
     def button_validate(self):
-        res = super(StockPicking, self).button_validate()
         if self.picking_type_id.code == "internal":
             for line in self.move_lines:
                 product_name = line.product_id.display_name
                 if not line.product_id.type == 'product':
                     raise Warning(u"El producto {} debe ser de tipo Almacenable.".format(product_name))
                 if self.location_id.id:
-                    obj_sq = self.env['stock.quant'].search([
-                                                            ('location_id', '=', self.location_id.id),
-                                                            ('product_id', '=', line.product_id.id)
-                                                            ])
-                    product_stock_qty = sum([x.quantity for x in obj_sq if x.quantity])
+                    product_stock_qty = line.product_id.with_context({'location': self.location_id.id}).qty_available
                     product_qty = line.quantity_done
                     name_warehouse = self.location_id.name
-                    if product_qty > product_stock_qty and self.picking_type_id.code != 'incoming':
-                        messaje = u"Usted planea transferir {} {} pero solo tiene {} en {} ".format(product_qty, product_name, product_stock_qty, name_warehouse)
+                    if product_qty > product_stock_qty:
+                        messaje = u"111Usted planea transferir {} {} pero solo tiene {} en {} ".format(product_qty, product_name, product_stock_qty, name_warehouse)
                         raise Warning(messaje)
 
             for line in self.move_line_ids:
@@ -78,15 +64,11 @@ class StockPicking(models.Model):
                 if not line.product_id.type == 'product':
                     raise Warning(u"El producto {} debe ser de tipo Almacenable.".format(product_name))
                 if self.location_id.id:
-                    obj_sq = self.env['stock.quant'].search([
-                                                            ('location_id', '=', self.location_id.id),
-                                                            ('product_id', '=', line.product_id.id)
-                                                            ])
-                    product_stock_qty = sum([x.quantity for x in obj_sq if x.quantity])
+                    product_stock_qty = line.product_id.with_context({'location': self.location_id.id}).qty_available
                     product_qty = line.qty_done
                     name_warehouse = self.location_id.name
                     if product_qty > product_stock_qty and self.picking_type_id.code != 'incoming':
                         messaje = u"Usted planea transferir {} {} pero solo tiene {} en {} ".format(product_qty, product_name, product_stock_qty, name_warehouse)
                         raise Warning(messaje)
 
-        return res
+        return super(StockPicking, self).button_validate()
